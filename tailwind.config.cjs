daa48ee7f28b3172f0592b0945a3b0427e08b75b
Jake Boldt <boldtjake573@gmail.com>
2025-10-25 12:43:51 -0500

    Applied Inter as site default + Bebas Neue for headers

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,2 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = { content: ["./index.html","./src/**/*.{js,jsx,ts,tsx}"], theme: { extend: {} }, plugins: [] };
+module.exports = { content: ["./index.html","./src/**/*.{js,jsx,ts,tsx}"], theme: { extend: {extend: {
+  fontFamily: {
+    sans: ['Inter', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+    display: ['Bebas Neue', 'Inter', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+  },
+},
+} }, plugins: [] };
